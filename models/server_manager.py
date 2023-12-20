5dfa2b98ee4789ddcdeac2ea54c17bcce6f32a42
sidney092k@gmail.com <sidney092k@gmail.com>
2023-12-20 01:47:15 -0300

    feat: chat ou video
    Co-authored-by: Peedrooo <pedrovitora.jesus@gmail.com>

---
+++ models/server_manager.py
@@ -0,0 +1,50 @@
+import os
+import json
+import socket
+
+class ServerManager():
+    
+    def enter_server(self):
+            self.mode = None
+            os.system('cls||clear')
+            with open('servers.json') as f:
+                data = json.load(f)
+            print('Servers: ', end="")
+            for servers in data:
+                print(servers, end=" ")
+            print('\nServer Name:',end=" ")
+            server_name = input()
+            print('Nickname:', end=" ")
+            self.nickname = input()
+            if self.nickname == 'admin':
+                print("Enter Password for Admin:", end=" ")
+                self.password = input()
+            self.comm_mode()
+            ip = data[server_name]["ip"]
+            port = data[server_name]["port"]
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((ip, port))
+
+    def comm_mode(self):
+        while True:
+            os.system('cls||clear')
+            option = input("Ativar câmera?:\n(1)Não\n(2)Sim\n")
+            option = int(option)
+            if option == 1:
+                self.mode = 'chat'
+                break
+            elif option == 2:
+                self.mode = 'video'
+                break
+    
+    def add_server(self):
+            os.system('cls||clear')
+            server_name = input("Enter a name for the server:")
+            server_ip = input("Enter the ip address of the server:")
+            server_port = int(input("Enter the port number of the server:"))
+
+            with open('src/resources/servers.json', 'r') as f:
+                data = json.load(f)
+            with open('src/resources/servers.json', 'w') as f:
+                data[server_name] = {"ip": server_ip, "port": server_port}
+                json.dump(data, f, indent=4)
