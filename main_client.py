3ad10e79ceffa56dd8d4945da4b75856769be3e2
sidney092k@gmail.com <sidney092k@gmail.com>
2023-12-20 14:45:12 -0300

    feat: web chat

--- main_client.py
+++
@@ -1,3 +0,0 @@
-from src.models.client import Client
-
-Client().start()
--- main_server.py
+++
@@ -1,8 +0,0 @@
-from src.models.server import Server
-
-server = Server(
-    host = "172.16.1.163",
-    qnt_users = 10
-)
-
-server.start()
